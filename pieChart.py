f74cd14fca0b1d413b985b74db3a27110e9512d5
nadahkhaledd <nadahkhaledd@gmail.com>
2022-04-26 01:15:03 +0200

    pie chart

--- pieChart.py
+++ pieChart.py
@@ -3,5 +3,6 @@ import matplotlib.pyplot as plt
 pLanguages = 'Python', 'C++', 'Java', 'Javascript', 'Ruby', 'R'
 sizes = [33, 52, 62, 48, 12, 17]
 
-plt.pie(sizes, labels=pLanguages)
+plt.pie(sizes, labels=pLanguages, autopct='%1.1f%%')
+plt.axis('equal')
 plt.show()
