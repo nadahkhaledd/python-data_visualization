ea80e7c683bdcef97bda72fec10a3bd510090182
nadahkhaledd <nadahkhaledd@gmail.com>
2022-04-26 00:40:31 +0200

    more plotting

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ years = [1950, 1955, 1960, 1965, 1970, 1975, 1980, 1985, 1990, 1995, 2000, 2005,
 population = [2.5, 2.7, 3, 3.3, 3.6, 4, 4.4, 4.8, 5.3, 5.7, 6.1, 6.5, 6.9, 7.3]
 
 
-plt.plot(years, population)
+plt.plot(years, population, color='red')
+plt.title('Population Growth')
+plt.xlabel('years')
+plt.ylabel('population (billion)')
 plt.show()
 
 
