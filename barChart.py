22e7e2a9f9ff31ead16d9725a2ebb8f9e6750d2e
nadahkhaledd <nadahkhaledd@gmail.com>
2022-04-26 01:53:19 +0200

    plot bar chart

--- barChart.py
+++ barChart.py
@@ -10,8 +10,12 @@ china_scores = (613, 570, 580)
 france_scores = (495, 505, 499)
 
 index = np.arange(col_count)
-
-k1 = plt.bar(index, korea_scores, .5)
+bar_width = 0.1
+k1 = plt.bar(index, korea_scores, bar_width)
+c1 = plt.bar(index + 0.1, canada_scores, bar_width)
+ch1 = plt.bar(index + 0.2, china_scores, bar_width)
+f1 = plt.bar(index + 0.3, france_scores, bar_width)
+plt.grid(True)
 plt.show()
 
 
