5eb5a39f448d070ba66a8c83a2f714fcd98299f7
nadahkhaledd <nadahkhaledd@gmail.com>
2022-04-26 01:39:36 +0200

    plot dataframes

--- practice_pandas.py
+++ practice_pandas.py
@@ -7,6 +7,11 @@ raw_data = {'names': ['Nick', 'Jessie', 'Robin', 'Marc', 'Jimmy'],
             'march_ir': [65, 88, 12, 32, 65]}
 
 df = pd.DataFrame(raw_data, columns=['names', 'jan_ir', 'feb_ir', 'march_ir'])
+df['total_ir'] = df['jan_ir'] + df['feb_ir'] + df['march_ir']
 print(df)
+
+plt.pie(df['total_ir'], labels=df['names'], autopct='%1.1f%%')
+plt.show()
+
 
 
