0cb547ef640bf6381389370f780eed9c70d9c291
nadahkhaledd <nadahkhaledd@gmail.com>
2022-04-26 02:09:54 +0200

    add bar chart labels

--- barChart.py
+++ barChart.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 col_count = 3
 
 korea_scores = (554, 536, 538)
@@ -11,11 +10,25 @@ france_scores = (495, 505, 499)
 
 index = np.arange(col_count)
 bar_width = 0.1
-k1 = plt.bar(index, korea_scores, bar_width)
-c1 = plt.bar(index + 0.1, canada_scores, bar_width)
-ch1 = plt.bar(index + 0.2, china_scores, bar_width)
-f1 = plt.bar(index + 0.3, france_scores, bar_width)
-plt.grid(True)
+
+k1 = plt.bar(index, korea_scores, bar_width,
+             alpha=.6,
+             label="Korea")
+c1 = plt.bar(index + 0.1, canada_scores, bar_width,
+             alpha=.6,
+             label="Canada")
+ch1 = plt.bar(index + 0.2, china_scores, bar_width,
+              alpha=.6,
+              label="China")
+f1 = plt.bar(index + 0.3, france_scores, bar_width,
+             alpha=.6,
+             label="France")
+
+
+plt.ylabel("Mean score in PISA 2012")
+plt.xlabel("Subjects")
+plt.title("Test Scores by Country")
+plt.xticks(index + .3 / 2, ("Mathematics", "Reading","Science"))
+plt.legend()
+#plt.grid(True)
 plt.show()
-
-
